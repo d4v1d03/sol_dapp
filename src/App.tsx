e2f9e96fbd9f7b58ea0bf1afdfb3f6903a16ef2b
Amit19057120 <amit08072005@gmail.com>
2023-03-06 13:50:04 +0530

    0.5

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './MintToken';
+import MintToken from './MintToken';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MintToken/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
