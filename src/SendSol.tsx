40afb50f4bf504de9c8eeee6bdf74b54133e9631
Amit19057120 <amit08072005@gmail.com>
2023-03-23 05:45:20 +0530

    finalized

--- src/SendSol.tsx
+++ src/SendSol.tsx
@@ -1,41 +1,121 @@
 import { 
     clusterApiUrl, 
     Connection, 
-    PublicKey, 
+    Transaction, 
     Keypair, 
+    PublicKey, 
     LAMPORTS_PER_SOL, 
-    sendAndConfirmTransaction,
-    Transaction
+    sendAndConfirmTransaction, 
+    SystemProgram 
 } from '@solana/web3.js';
-
-import { 
-    createMint, 
-    getOrCreateAssociatedTokenAccount, 
-    mintTo,  
-    Account, 
-    createSetAuthorityInstruction,
-    AuthorityType
+import { getAccount, 
+    getAssociatedTokenAddress, 
+    NATIVE_MINT, 
+    createAssociatedTokenAccountInstruction, 
+    createSyncNativeInstruction, 
+    transfer, 
+    getOrCreateAssociatedTokenAccount,
+    closeAccount
 } from '@solana/spl-token';
 
-window.Buffer = window.Buffer || require("buffer").Buffer;
-
-function SendSol(){
+function SendSol() {    
     const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
     const fromWallet = Keypair.generate();
-    let fromTokenAccount: Account; 
-	let mint: PublicKey;
+    let associatedTokenAccount: PublicKey;
 
+    async function wrapSol() {
+        const airdropSignature = await connection.requestAirdrop(
+            fromWallet.publicKey,
+            2 * LAMPORTS_PER_SOL,
+        );
+
+        await connection.confirmTransaction(airdropSignature);
+        associatedTokenAccount = await getAssociatedTokenAddress(
+            NATIVE_MINT,
+            fromWallet.publicKey
+        );
+
+        const ataTransaction = new Transaction().add(
+            createAssociatedTokenAccountInstruction(
+                fromWallet.publicKey,
+                associatedTokenAccount,
+                fromWallet.publicKey,
+                NATIVE_MINT
+            )
+        );
+
+        await sendAndConfirmTransaction(connection, ataTransaction, [fromWallet]);
+
+        const solTransferTransaction = new Transaction()
+        .add(
+            SystemProgram.transfer({
+                fromPubkey: fromWallet.publicKey,
+                toPubkey: associatedTokenAccount,
+                lamports: LAMPORTS_PER_SOL // the SOL that we're sending over
+            }),
+            createSyncNativeInstruction(
+                associatedTokenAccount
+            )
+        );
+
+        await sendAndConfirmTransaction(connection, solTransferTransaction, [fromWallet]);
+        const accountInfo = await getAccount(connection, associatedTokenAccount);
+        console.log(`Native: ${accountInfo.isNative}, Lamports: ${accountInfo.amount}`);
+    }
     
+    async function unwrapSol() {
+        const walletBalance = await connection.getBalance(fromWallet.publicKey);
+        console.log(`Balance before unwrapping WSOL: ${walletBalance}`)
+        await closeAccount(
+            connection, 
+            fromWallet, 
+            associatedTokenAccount, 
+            fromWallet.publicKey, 
+            fromWallet
+        );
+        const walletBalancePostClose = await connection.getBalance(fromWallet.publicKey);
+        console.log(`Balance after unwrapping WSOL: ${walletBalancePostClose}`)
+    }
 
-    return(
-    <div>
-        Send SOL Section
+    async function sendSol() {
+        const fromAirdropSignature = await connection.requestAirdrop(fromWallet.publicKey, LAMPORTS_PER_SOL);
+
+        await connection.confirmTransaction(fromAirdropSignature);
+        const toWallet = new PublicKey("9WoeerdzrFVKi5NWYZkCMmWdV1PK3upARSLnFUfeXoBG")
+
+        // Get the token account of the fromWallet address, and if it does not exist, create it
+        const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+            connection,
+            fromWallet,
+            NATIVE_MINT,
+            fromWallet.publicKey
+        );
+
+        // Get the token account of the toWallet address, and if it does not exist, create it
+        const toTokenAccount = await getOrCreateAssociatedTokenAccount(connection, fromWallet, NATIVE_MINT, toWallet);
+
+        // Transfer the new token to the "toTokenAccount" we just created
+        const signature = await transfer(
+            connection,
+            fromWallet,
+            fromTokenAccount.address,
+            toTokenAccount.address,
+            fromWallet.publicKey,
+            LAMPORTS_PER_SOL
+        );
+        console.log('Transfer tx:', signature);
+    }
+
+    return (
         <div>
-            <button /*onClick={createNft}*/>Create NFT</button>
-            <button /*onClick={mintNft}*/>Mint NFT</button>
-            <button /*onClick={lockNft}*/>Lock NFT</button>
+            Send Sol Section
+            <div>
+                <button onClick={wrapSol}>Wrap SOL</button>
+                <button onClick={unwrapSol}>Unwrap SOL</button>
+                <button onClick={sendSol}>Send SOL</button>
+            </div>
         </div>
-    </div>
     );
 }
+
 export default SendSol;
